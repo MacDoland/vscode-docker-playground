1f166e7c5d08187ae87261553f37b5e348fc41ac
MacDoland <joe.mcdowall@gmail.com>
2020-06-17 09:53:25 +0100

    enabling remote debugging

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  const rand = Math.floor(Math.random() * 10000)
+  ctx.body = `Hello World: ${rand}`;
 });
 
 app.listen(3000);
